ab0c201e906cd62f788dfb23c3a3e0d6873f8839
Xymond Louisse M. Alcazar <2227226@slu.edu.ph>
2024-10-12 11:55:35 +0800

    started extract_info.sh, only finished the center_text() method.

--- extract_info.sh
+++ extract_info.sh
@@ -2,6 +2,7 @@
 #==================================================
 # Script Name:		extract_info.sh
 # By:			Xymond Louisse M. Alcazar
+# Group Name:		Team Lemar
 # Purpose:		a method that extracts
 #			specific informaiton in the
 #			products dataset
@@ -9,4 +10,10 @@
 #==================================================
 products=~/res/products.txt
 
-
+#to make the display centered
+center_text() {
+	local text=$1
+	local width=$(tput cols)
+	local spaces=$((width / 3))
+	printf "%${spaces}s%s\n" "" "$text"
+}
