6ec9d2be948b1a3ad02fa7f3bfbd3a02bb7a1ad0
Carl Angelo Espartinas <2244405@slu.edu.ph>
2024-10-16 10:49:23 +0800

    Add script in search_product.sh

---
+++ search_product.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#==========================================================
+#Author		: 	Espartinas, Carl Angelo B.
+#File Name 	: 	search_product.sh
+#Purpose	: 	Allow User to search for specified 
+#			product in the inventory
+#==========================================================
+
+products=~/res/IPdataset.txt
+
+center_text=~/my_funcs
