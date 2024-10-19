a15114aae394844db8eca0169402a689e7d1c040
Carl Angelo Espartinas <2244405@slu.edu.ph>
2024-10-19 11:54:10 +0800

    updated the search product again

--- search_product.sh
+++ search_product.sh
@@ -10,15 +10,14 @@ DATA_FILE=./res/IPSdataset.txt
 
 source ./res/my_funcs
 clear
-
+echo -e "\n\n\n\n\n"
  # Check if the data file exists
     if [ ! -f "$DATA_FILE" ]; then
         echo "Data file not found!"
     fi
 
     # Prompt user for search input
-    center_text "Enter search term (Product ID, Name, Category, Supplier):"
-    read search_term
+    read -p "$(center_text 'Enter search term (Product ID, Name, Category, Supplier): ')" search_term
 
     # Search and display matching products
     center_text "Searching for '$search_term'..."
@@ -32,14 +31,14 @@ clear
 	center_text "================================"
         center_text "Matching products:"
 	center_text "================================"
-        center_text "$results" | while IFS=: read -r product_id name category supplier price quantity sales; do
-            center_text "Product ID: $product_id"
-            center_text "Name: $name"
-            center_text "Category: $category"
-            center_text "Supplier: $supplier"
-            center_text "Price: $price"
-            center_text "Quantity: $quantity"
-            center_text "Sales: $sales"
+        center_text "$results" | while IFS=':' read -r product_id name category supplier price quantity sales; do
+            center_text "Product ID	: $product_id"
+            center_text "Name		: $name"
+            center_text "Category		: $category"
+            center_text "Supplier		: $supplier"
+            center_text "Price		: $price"
+            center_text "Quantity		: $quantity"
+            center_text "Sales		: $sales"
             center_text "---------------------------------------"
         done
     fi
